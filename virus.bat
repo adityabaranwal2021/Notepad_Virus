af284ea2a507b606c7f34e9050b84f476099f977
ADITYA BARANWAL <95960866+adityabaranwal2021@users.noreply.github.com>
2023-04-02 17:10:33 +0530

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,10 @@
+Pause
+@echo off
+color 2
+mode 700
+
+:s
+
+echo %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%
+
+goto s
